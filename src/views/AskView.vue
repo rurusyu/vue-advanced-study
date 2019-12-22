05313ee5981bfbea1f0f8ca632574fa9f3c6a3b3
leo.kwon <zjinsez@gmail.com>
2019-12-22 16:55:19 +0900

    뷰정리

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for="ask in fetchedAsk">{{ ask.title }}</div>
+    <p v-for="item in fetchedAsk">
+      <a :href="item.url"> {{ item.title }} </a>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
+    </p>
   </div>
 </template>
 <script>
