547852ea5f224fb32e0424c8df8221a35f468372
rurusyu <Charizard006@protonmail.com>
2019-12-24 16:47:47 +0900

    UserProfile, UserView 파일 생성

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="user-container">
+    <div>User</div>
+    <div class="user-description">
+      <div>{{info.id}}</div>
+      <div class="time">{{info.created}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+//1.자식컴포넌트에서 데이터 접근
+//2. 상위 컴포넌트에서 props로 전달 받음/
+export default {
+  props: {
+    info: Object //Object는 Props validation
+  }
+  //1.번 방법
+  //   computed: {
+  //     userInfo() {
+  //       return this.$store.state.user;
+  //     }
+  //   }
+};
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+}
+
+.user-description {
+  padding: 8px;
+}
+.time {
+  font-size: 12px;
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
-   <p>name: {{userInfo.id}}</p>
-   <p>karma: {{userInfo.karma}}</p>
-   <p>created: {{userInfo.created}}</p>
+    <user-profile :info="userInfo"></user-profile>
   </div>
 </template>
 <script>
+import UserProfile from "../components/UserProfile.vue";
 export default {
-    computed:{
-        userInfo(){
-            return this.$store.state.user
-        }
-    },
-    
-    created(){
-        const userName = this.$route.params.id;
-        console.log(userName);
-        this.$store.dispatch('FETCH_USER',userName);  //payload는 하나만 가능, 여러개 보낼시 객체로 감싸서 보내면됨, 이럴떄 디스트럭쳐링이 좋음
+  components: {
+    UserProfile
+  },
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
     }
+  },
+
+  created() {
+    const userName = this.$route.params.id;
+    this.$store.dispatch("FETCH_USER", userName); //payload는 하나만 가능, 여러개 보낼시 객체로 감싸서 보내면됨, 이럴떄 디스트럭쳐링이 좋음
+  }
 };
 </script>
 <style></style>
