9a5d20455d153ed6c4d6b6904b2f8ddfedfe5cae
rurusyu <Charizard006@protonmail.com>
2019-12-24 15:15:15 +0900

    v-if를 이용한 공통 컴포넌트 화

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,8 +1,83 @@
-<template></template>
+<template>
+  <div>
+    <ul class="news-list">
+      <li class="post" v-for="item in listItems">
+        <div class="points">{{item.points || 0}}</div>
 
+        <!-- 기타 정보영역 -->
+        <div>
+          <!-- 타이틀 영역 -->
+          <p class="news-title">
+            <template v-if="item.domain">
+              <a :href="item.url">{{ item.title }}</a>
+            </template>
+            <template v-else>
+              <router-link :to="`item/${item.id}`">{{item.title}}</router-link>
+            </template>
+          </p>
+          <small class="link-text">
+            {{item.time_ago}}
+            by
+            <router-link v-if="item.user" class="link-text" :to="'/user/'+item.user">{{ item.user }}</router-link>
+            <a v-else :href="item.url">{{ item.domain }}</a>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
 <script>
-export default {};
+export default {
+  computed: {
+    listItems() {
+      const name = this.$route.name;
+      if (name === "news") {
+        return this.$store.state.news;
+      } else if (name === "ask") {
+        return this.$store.state.asks;
+      } else if (name === "jobs") {
+        return this.$store.state.jobs;
+      }
+    }
+  },
+  created() {
+    //공통 컴포넌트 api call 분기처리
+    const name = this.$route.name;
+    let actionName;
+    if (name === "news") {
+      actionName = "FETCH_NEWS";
+    } else if (name === "ask") {
+      actionName = "FETCH_ASK";
+    } else if (name === "jobs") {
+      actionName = "FETCH_JOB";
+    }
+    this.$store.dispatch(actionName);
+  }
+};
 </script>
-
-<style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
